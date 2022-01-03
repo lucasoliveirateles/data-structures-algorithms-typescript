621f138fbb61bb765c85b0d42745106513330a9d
Lucas Teles <dev.teles@outlook.com>
2022-01-03 03:14:57 -0300

    creating array and making interation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Data Structures And Algorithms
+  * Creating array and making interation
+  * Inserting a element to the end of array
---
+++ array/creating-array-making-interation.ts
@@ -0,0 +1,14 @@
+/* Creating array and making interation */
+const daysOfWeek: string[] = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+
+for (let i = 0; i < daysOfWeek.length; i++) {
+  console.log(daysOfWeek[i]);
+}
