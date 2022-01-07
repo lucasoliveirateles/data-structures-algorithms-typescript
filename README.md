e298e55ca04dbf966c2b72ae7497258d98b5f373
Lucas Teles <dev.teles@outlook.com>
2022-01-07 06:37:40 -0300

    removing element to the end of array

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
   * Inserting a element to the start of array
   * Joining arrays
   * Ordering strings
+  * Removing element to the end of array
---
+++ array/removing-element-end-array.ts
@@ -0,0 +1,6 @@
+/* Removing element to the end of array */
+const numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.pop();
+
+console.log(numbers);
